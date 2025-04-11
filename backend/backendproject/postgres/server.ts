40c789b3c5bba06ef1e130a8f2b1e25cd32e49ed
Shivansh pachauri <shivanshpachauri00@gmail.com>
2025-04-11 19:30:42 +0530

    added styling

--- backend/backendproject/postgres/server.ts
+++ backend/backendproject/postgres/server.ts
@@ -1,11 +1,5 @@
 import pg from "npm:pg";
-// export const pool1 = new pg.Pool({
-//   user: Deno.env.get("pg_user"),
-//   host: Deno.env.get("pg_host"),
-//   database: Deno.env.get("pg_database"),
-//   password: Deno.env.get("pg_password"),
-//   port: Deno.env.get("pg_port"),
-// });
+
 export const pool1 = new pg.Pool({
   connectionString: Deno.env.get("neon_db_connection_string"),
   ssl: { rejectUnauthorized: false },
--- frontend/AshuMedical/src/components/Http/Axios.js
+++ frontend/AshuMedical/src/components/Http/Axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 // import.meta.env.VITE_API_URL ||
-// const API_BASE_URL = "http://localhost:3000/";
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-console.trace(API_BASE_URL);
+const API_BASE_URL = "http://localhost:3000/";
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
